f02252cfdd7f1e743dbefd08f1c7482ce6b38f36
Heeyun0724 <90318181+Heeyun0724@users.noreply.github.com>
2024-02-15 11:22:24 +0900

    Update video hand detection coordinate.py

--- video hand detection coordinate.py
+++ video hand detection coordinate.py
@@ -2,17 +2,18 @@ import json
 import cv2
 import mediapipe as mp
 
+# Mediapipe Hands 모듈 로드
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
+# 트랙바 이벤트 핸들러 함수
 def onChange(pos):
     cap.set(cv2.CAP_PROP_POS_FRAMES, pos)
 
+# 비디오에서 손을 감지하는 함수
 def detect_hands_in_video(cap):
     landmarks_data_left = [[] for _ in range(21)]  # 21개의 랜드마크를 가집니다.
     landmarks_data_right = [[] for _ in range(21)]  # 21개의 랜드마크를 가집니다.
-    #fps = 5  # 초당 5프레임으로 설정
-    #cap.set(cv2.CAP_PROP_FPS, fps)  # 초당 프레임 수 설정
 
     with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=2) as hands:
         while cap.isOpened():
@@ -22,20 +23,21 @@ def detect_hands_in_video(cap):
             if not ret:
                 break
 
-            # Convert the BGR image to RGB.
+            # BGR 이미지를 RGB로 변환합니다.
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # Process the image and find the hand landmarks.
+            # 이미지를 처리하고 손 랜드마크를 찾습니다.
             results = hands.process(image)
 
-            # Draw the hand landmarks on the image.
+            # 이미지에 손 랜드마크를 그립니다.
             if results.multi_hand_landmarks:
                 hands_count = len(results.multi_hand_landmarks)
                 for hand_landmarks in results.multi_hand_landmarks:
                     mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-                for id in range(21):  # For each landmark
+                for id in range(21):  # 각 랜드마크에 대해
                     if hands_count == 2:
+                        # 두 손이 감지된 경우
                         if results.multi_hand_landmarks[0].landmark[0].x > results.multi_hand_landmarks[1].landmark[0].x:
                             left_hand = results.multi_hand_landmarks[0]
                             right_hand = results.multi_hand_landmarks[1]
@@ -48,9 +50,8 @@ def detect_hands_in_video(cap):
                         landmarks_data_left[id].append(coordinate_left)
                         landmarks_data_right[id].append(coordinate_right)
                         
-                    # If only one hand is detected
                     elif hands_count == 1:  
-                        # Determine if the hand is left or right based on the position of the thumb and pinky
+                        # 한 손만 감지된 경우
                         if results.multi_hand_landmarks[0].landmark[4].x < results.multi_hand_landmarks[0].landmark[20].x:
                             coordinate_right = [results.multi_hand_landmarks[0].landmark[id].x, results.multi_hand_landmarks[0].landmark[id].y, results.multi_hand_landmarks[0].landmark[id].z]
                             landmarks_data_right[id].append(coordinate_right)
@@ -60,12 +61,12 @@ def detect_hands_in_video(cap):
                             landmarks_data_left[id].append(coordinate_left)
                             landmarks_data_right[id].append([])
                             
-                     # If no hands are detected        
                     else: 
+                        # 손이 감지되지 않은 경우
                         landmarks_data_left[id].append([])
                         landmarks_data_right[id].append([])
                         
-            else:  # If no hands are detected
+            else:  # 손이 감지되지 않은 경우
                 for id in range(21):
                     landmarks_data_left[id].append([])
                     landmarks_data_right[id].append([])
@@ -75,19 +76,18 @@ def detect_hands_in_video(cap):
             if cv2.waitKey(5) & 0xFF == 27:
                 break
 
-
+    # JSON 파일로 데이터를 저장합니다.
     for id, data in enumerate(landmarks_data_left):
         with open(f'C:/Users/Desktop/KIST/landmark/landmarks_data_left_{id}.json', 'w') as f:
-            json.dump(data, f)  # 데이터를 JSON 형식으로 파일에 씁니다.
+            json.dump(data, f)
 
     for id, data in enumerate(landmarks_data_right):
         with open(f'C:/Users/Desktop/KIST/landmark/landmarks_data_right_{id}.json', 'w') as f:
-            json.dump(data, f)  # 데이터를 JSON 형식으로 파일에 씁니다.
-
+            json.dump(data, f)
 
     cv2.destroyAllWindows()
 
-# 비디오 경로를 자신의 경로로 변경
+# 비디오 경로를 설정합니다.
 cap = cv2.VideoCapture("C:/Users/Desktop/video/output.mp4")
 cv2.namedWindow('MediaPipe Hands')
 total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
