c9225c5fee590874f9d769ad0d7b1e77ae5366af
Heeyun0724 <90318181+Heeyun0724@users.noreply.github.com>
2024-02-15 11:20:51 +0900

    Update video hand detection Angle.py

--- video hand detection Angle.py
+++ video hand detection Angle.py
@@ -4,14 +4,14 @@ import numpy as np
 import json
 from tqdm import tqdm
 
+# Mediapipe Hands 모듈 로드
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-#추출하고 손의 랜드마크 id를 수정해야 함 
-# 현재에는 2,5,9,13,17의 각도를 구한 것
+# 추출하고 수정해야 하는 손의 랜드마크 id
 finger_landmarks = {'Thumb': [0, 2, 3], 'Index Finger': [0, 5, 6], 'Middle Finger': [0, 9, 10], 'Ring Finger': [0, 13, 14], 'Pinky': [0, 17, 18]}
 
-#손가락의 각도 구하는 함수
+# 손가락 각도를 계산하는 함수
 def calculate_angle(a, b, c):
     a = np.array(a)
     b = np.array(b)
@@ -27,11 +27,11 @@ def calculate_angle(a, b, c):
 
 both_hands_detected_frames = 0
 
-# 비디오 파일 경로 설정 (실제 파일 경로로 교체)
+# 비디오 파일 경로
 video_path = "C:/Users/Desktop/video/drive-download-20240124T011103Z-00.mp4"
 cap = cv2.VideoCapture(video_path)
 
-# 비디오 파일 여는데 실패한 경우 에러 출력 후 종료
+# 비디오 파일 열기
 if not cap.isOpened():
     print("비디오 파일을 열 수 없습니다.")
     exit()
@@ -40,19 +40,20 @@ if not cap.isOpened():
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-# mediapipe Hands 모델 초기화
+# Mediapipe Hands 모델 초기화
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
     frame_number = 0
 
-    # 각 손가락별로 JSON 파일을 열기 위한 딕셔너리 초기화 경로 설정 수정 
+    # 손가락별 JSON 파일을 열기 위한 딕셔너리 초기화
     json_files_left = {finger: open(f'C:/Users/Desktop/curve/landmarks_curve_left_{finger}.json', 'w') for finger in finger_landmarks}
     json_files_right = {finger: open(f'C:/Users/Desktop/curve/landmarks_curve_right_{finger}.json', 'w') for finger in finger_landmarks}
 
+    # 비디오 프레임 반복 처리
     for _ in tqdm(range(frame_count)):
         ret, frame = cap.read()
 
         if not ret:
-            break  # 비디오가 끝나면 루프 종료
+            break  
 
         frame_number += 1
 
@@ -92,6 +93,7 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
             left_hand = None
             right_hand = None
 
+        # 손가락 각도 계산
         for finger, points in finger_landmarks.items():
             if left_hand is not None:
                 angle_left = calculate_angle(
